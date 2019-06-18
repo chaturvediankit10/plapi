b0aa05a4fa6b6fceba66630073c7277b24082f26
WindGH <44694497+WindGH@users.noreply.github.com>
2019-06-18 23:15:10 +0800

    Fix the gem error caused by 0 points for now until better solution.

--- app/controllers/search_api/dashboard_controller.rb
+++ app/controllers/search_api/dashboard_controller.rb
@@ -842,8 +842,13 @@ class SearchApi::DashboardController < ApplicationController
     # number_days   = num_months * 30.0
     # return (((fees + interest_paid) / loan_amount) / number_days ) * 365.0 * 100.0
 
-    loan = FinanceMath::Loan.new(nominal_rate: air, duration: term * 12, amount: loan_amount, structure_fee: points, currency_protection: 0, fee: 0)
-    return loan.apr
+    if ( points == 0 )
+      return air
+    elsif points > 0
+      loan = FinanceMath::Loan.new(nominal_rate: air, duration: term * 12, amount: loan_amount, structure_fee: points, currency_protection: 0, fee: 0)
+      return loan.apr
+    else
+      return nil
   end
 
 end
