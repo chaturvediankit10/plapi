5cd860d3fbbc0513dc585016b8475b1021b48fe8
Virendra <virendrayuvasoft221@gmail.com>
2019-06-26 19:01:24 +0530

    Calculation Optimizaion

--- app/models/mathe_matics.rb
+++ app/models/mathe_matics.rb
@@ -1,5 +1,4 @@
 class MatheMatics
-
   class << self
     def digits(num)
       return Math.log10(num.to_i).to_i + 1
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,3 @@
 Rails.application.config.assets.precompile += %w( search_api/application.scss search_api/datatables.scss search_api/dashboard.scss search_api/custom_style.css search_api/application.js search_api/cable.js search_api/datatables.js )
+
+Rails.application.config.autoload_paths +=  %w("#{Rails.root}/lib") # add this line
