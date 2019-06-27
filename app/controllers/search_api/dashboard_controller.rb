1f7c63a2e76c4971d36a0d768d2aa4c15418edd3
Virendra <virendrayuvasoft221@gmail.com>
2019-06-27 16:58:17 +0530

    add method for Monthly Expenses Breakdown pie chart for calculator and mortgage

--- app/controllers/search_api/dashboard_controller.rb
+++ app/controllers/search_api/dashboard_controller.rb
@@ -533,7 +533,7 @@ class SearchApi::DashboardController < ApplicationController
           hash_obj[:air_point] = air_and_point_value['air_point']
 
           hash_obj[:apr] = calculate_apr_value( air, @term.to_i, loan_amount, air_and_point_value['air_point'] )
-          hash_obj[:monthly_breakdown] = SearchApi::Calculation.new.monthly_expenses_breakdown(loan_amount, (@term.to_i*12), hash_obj[:monthly_payment], @home_price.to_i, @default_annual_home_insurance, @default_pmi_insurance, @default_property_tax_perc, @down_payment)
+          hash_obj[:monthly_breakdown] = SearchApi::Calculation.new.monthly_expenses_breakdown(loan_amount, (@term.to_i*12), hash_obj[:monthly_payment], @home_price.to_i, @default_annual_home_insurance, @default_pmi_insurance, @default_property_tax_perc, @down_payment, params)
         end
       end
 
@@ -857,10 +857,6 @@ class SearchApi::DashboardController < ApplicationController
     return monthly_payment
   end
 
-  # def calculate_apr_value(air_value)
-  #   ( 1 + air_value / 30 ) ** 365 - 1 rescue nil
-  # end
-
   def calculate_apr_value( air, term, loan_amount, points )
     # num_months    = term * 12.0
     # fees          = points / 100.0 * loan_amount
@@ -877,55 +873,5 @@ class SearchApi::DashboardController < ApplicationController
       return nil
     end
   end
-
-  
-
-  # def monthly_expenses_breakdown(calculate_loan_payment, number_of_payments, calculate_monthly_payment, home_price, default_annual_home_insurance, default_pmi_insurance)
-  #   monthly_breakdown = {
-  #     :mortgage_principal => {},
-  #     :mortgage_interest => {},
-  #     :home_insurance => {},
-  #     :pmi_insurance => {},
-  #     :hoa_dues => {},
-  #     :monthly_expenses_sum => {},
-  #     :property_tax => {}
-  #   }
-
-  #   property_tax = {}
-  #   property_tax[:monthly] = (home_price * @default_property_tax_perc*1.0 /100/12)
-  #   property_tax[:total] = (property_tax[:monthly]*number_of_payments)
-  #   monthly_breakdown[:property_tax][:monthly] = property_tax[:monthly]
-  #   monthly_breakdown[:mortgage_principal][:monthly] = (calculate_loan_payment/number_of_payments)
-  #   monthly_breakdown[:mortgage_principal][:total] = calculate_loan_payment
-
-  #   monthly_breakdown[:mortgage_interest][:monthly] = (calculate_monthly_payment-monthly_breakdown[:mortgage_principal][:monthly])
-  #   monthly_breakdown[:mortgage_interest][:total] =  (calculate_monthly_payment*number_of_payments-monthly_breakdown[:mortgage_principal][:total])
-  #   monthly_breakdown[:home_insurance][:monthly] = (home_price*0.35).round(2)
-
-  #   monthly_breakdown[:home_insurance][:total] = ((default_annual_home_insurance*1.0*number_of_payments)/12)
-  #   monthly_breakdown[:pmi_insurance][:monthly] = default_pmi_insurance
-  #   monthly_breakdown[:pmi_insurance][:total] =  monthly_breakdown[:pmi_insurance][:monthly].to_i == 0 ? 0.0 :  monthly_breakdown[:pmi_insurance][:monthly]*calculate_pmi_term(home_price, number_of_payments)
-  #   monthly_breakdown[:hoa_dues][:monthly] = 0.00
-  #   monthly_breakdown[:hoa_dues][:total] = (monthly_breakdown[:hoa_dues][:monthly]*number_of_payments)
-  #   monthly_breakdown[:monthly_expenses_sum][:monthly] =  ((monthly_breakdown[:mortgage_principal][:monthly] + monthly_breakdown[:mortgage_interest][:monthly] + property_tax[:monthly] + monthly_breakdown[:home_insurance][:monthly] + monthly_breakdown[:pmi_insurance][:monthly] + monthly_breakdown[:hoa_dues][:monthly]))
-
-  #   monthly_breakdown[:monthly_expenses_sum][:total] = ((monthly_breakdown[:mortgage_principal][:total] + monthly_breakdown[:mortgage_interest][:total] + property_tax[:total] + monthly_breakdown[:home_insurance][:total] + monthly_breakdown[:pmi_insurance][:total]))
-
-  #   monthly_breakdown[:mortgage_principal][:percentage] = ((monthly_breakdown[:mortgage_principal][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).infinite? ? 0.0 : ((monthly_breakdown[:mortgage_principal][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
-
-  #   monthly_breakdown[:mortgage_interest][:percentage] =  ((monthly_breakdown[:mortgage_interest][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).infinite? ? 0.0 : ((monthly_breakdown[:mortgage_interest][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
-
-  #   property_tax[:percentage] = ((property_tax[:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
-
-  #   monthly_breakdown[:home_insurance][:percentage] = ((monthly_breakdown[:home_insurance][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
-
-  #   monthly_breakdown[:pmi_insurance][:percentage] =  ((monthly_breakdown[:pmi_insurance][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
-
-  #   monthly_breakdown[:hoa_dues][:percentage] =  ((monthly_breakdown[:hoa_dues][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
-
-  #   monthly_breakdown[:monthly_expenses_sum][:percentage] = (monthly_breakdown[:mortgage_principal][:percentage] + monthly_breakdown[:mortgage_interest][:percentage] + property_tax[:percentage] + monthly_breakdown[:home_insurance][:percentage] + monthly_breakdown[:pmi_insurance][:percentage] + monthly_breakdown[:hoa_dues][:percentage]).round()
-
-  #   return monthly_breakdown
-  # end
 
 end
--- lib/search_api/calculation.rb
+++ lib/search_api/calculation.rb
@@ -1,49 +1,70 @@
 module SearchApi
   class Calculation
-    def monthly_expenses_breakdown(calculate_loan_payment, number_of_payments, calculate_monthly_payment, home_price, default_annual_home_insurance, default_pmi_insurance, default_property_tax_perc, down_payment)
-      monthly_breakdown = {
-        :mortgage_principal => {},
-        :mortgage_interest => {},
-        :home_insurance => {},
-        :pmi_insurance => {},
-        :hoa_dues => {},
-        :monthly_expenses_sum => {},
-        :property_tax => {}
-      }
+    def monthly_expenses_breakdown(calculate_loan_payment, number_of_payments, calculate_monthly_payment, home_price, default_annual_home_insurance, default_pmi_insurance, default_property_tax_perc, down_payment, params)
+      mortgage_principal = {}
+      mortgage_interest  = {}
+      home_insurance = {}
+      @pmi_insurance = {}
+      hoa_dues = {}
+      monthly_expenses_sum = {}
       property_tax = {}
-      property_tax[:monthly] = (home_price * default_property_tax_perc*1.0 /100/12)
-      property_tax[:total] = (property_tax[:monthly]*number_of_payments)
-      monthly_breakdown[:property_tax][:monthly] = property_tax[:monthly]
-      monthly_breakdown[:mortgage_principal][:monthly] = (calculate_loan_payment/number_of_payments)
-      monthly_breakdown[:mortgage_principal][:total] = calculate_loan_payment
-      monthly_breakdown[:mortgage_interest][:monthly] = (calculate_monthly_payment-monthly_breakdown[:mortgage_principal][:monthly])
-      monthly_breakdown[:mortgage_interest][:total] =  (calculate_monthly_payment*number_of_payments-monthly_breakdown[:mortgage_principal][:total])
-      monthly_breakdown[:home_insurance][:monthly] = (home_price*0.35).round(2)
 
-      monthly_breakdown[:home_insurance][:total] = ((default_annual_home_insurance*1.0*number_of_payments)/12)
-      monthly_breakdown[:pmi_insurance][:monthly] = default_pmi_insurance
-      monthly_breakdown[:pmi_insurance][:total] =  monthly_breakdown[:pmi_insurance][:monthly].to_i == 0 ? 0.0 :  monthly_breakdown[:pmi_insurance][:monthly]*calculate_pmi_term(home_price, number_of_payments, down_payment)
-      monthly_breakdown[:hoa_dues][:monthly] = 0.00
-      monthly_breakdown[:hoa_dues][:total] = (monthly_breakdown[:hoa_dues][:monthly]*number_of_payments)
-      monthly_breakdown[:monthly_expenses_sum][:monthly] =  ((monthly_breakdown[:mortgage_principal][:monthly] + monthly_breakdown[:mortgage_interest][:monthly] + property_tax[:monthly] + monthly_breakdown[:home_insurance][:monthly] + monthly_breakdown[:pmi_insurance][:monthly] + monthly_breakdown[:hoa_dues][:monthly]))
+      if params["monthly_property_tax"].present?
+        property_tax[:monthly] = params["monthly_property_tax"].delete(',').to_f rescue 0.0
+        property_tax[:total] = (property_tax[:monthly]*number_of_payments) rescue 0.0
+      else
+        property_tax[:monthly] = (home_price * default_property_tax_perc*1.0 /100/12) rescue 0.0
+        property_tax[:total] = (property_tax[:monthly]*number_of_payments) rescue 0.0
+      end
 
-      monthly_breakdown[:monthly_expenses_sum][:total] = ((monthly_breakdown[:mortgage_principal][:total] + monthly_breakdown[:mortgage_interest][:total] + property_tax[:total] + monthly_breakdown[:home_insurance][:total] + monthly_breakdown[:pmi_insurance][:total]))
+      mortgage_principal[:monthly] = (calculate_loan_payment/number_of_payments) rescue 0.0
+      mortgage_principal[:total] = calculate_loan_payment
 
-      monthly_breakdown[:mortgage_principal][:percentage] = ((monthly_breakdown[:mortgage_principal][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).infinite? ? 0.0 : ((monthly_breakdown[:mortgage_principal][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
+      mortgage_interest[:monthly] = (calculate_monthly_payment-mortgage_principal[:monthly]) rescue 0.0
+      mortgage_interest[:total] =  (calculate_monthly_payment*number_of_payments-mortgage_principal[:total]) rescue 0.0
+      if params["commit"].nil? && params["monthly_home_insurance"].present?
+        home_insurance[:monthly] = params["monthly_home_insurance"].delete(',').to_f
+      else
+        home_insurance[:monthly] = (home_price*0.35).round(2) rescue 0.0
+      end
 
-      monthly_breakdown[:mortgage_interest][:percentage] =  ((monthly_breakdown[:mortgage_interest][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).infinite? ? 0.0 : ((monthly_breakdown[:mortgage_interest][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
+      home_insurance[:total] = ((default_annual_home_insurance*1.0*number_of_payments)/12) rescue 0.0
 
-      property_tax[:percentage] = ((property_tax[:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
+      if params["commit"].nil? && params["monthly_pmi_insurance"].present?
+        @pmi_insurance[:monthly] = params["monthly_pmi_insurance"].delete(',').to_f
+        default_pmi_insurance = params["monthly_pmi_insurance"].delete(',').to_f
+      else
+        @pmi_insurance[:monthly] = default_pmi_insurance rescue 0.0
+      end
+      @pmi_insurance[:total] =  @pmi_insurance[:monthly].to_i == 0 ? 0.0 :  @pmi_insurance[:monthly]*calculate_pmi_term rescue 0.0
 
-      monthly_breakdown[:home_insurance][:percentage] = ((monthly_breakdown[:home_insurance][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
+      if params["commit"].nil? && params["monthly_hoa_dues"].present?
+        hoa_dues[:monthly] = params["monthly_hoa_dues"].delete(',').to_f
+      else
+        hoa_dues[:monthly] = 0.00
+      end
 
-      monthly_breakdown[:pmi_insurance][:percentage] =  ((monthly_breakdown[:pmi_insurance][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
+      hoa_dues[:total] = (hoa_dues[:monthly]*number_of_payments) rescue 0.0
 
-      monthly_breakdown[:hoa_dues][:percentage] =  ((monthly_breakdown[:hoa_dues][:monthly]*100 / monthly_breakdown[:monthly_expenses_sum][:monthly])).round(2)
+      monthly_expenses_sum[:monthly] =  ((mortgage_principal[:monthly] + mortgage_interest[:monthly] + property_tax[:monthly] + home_insurance[:monthly] + @pmi_insurance[:monthly] + hoa_dues[:monthly]))  rescue 0.0
 
-      monthly_breakdown[:monthly_expenses_sum][:percentage] = (monthly_breakdown[:mortgage_principal][:percentage] + monthly_breakdown[:mortgage_interest][:percentage] + property_tax[:percentage] + monthly_breakdown[:home_insurance][:percentage] + monthly_breakdown[:pmi_insurance][:percentage] + monthly_breakdown[:hoa_dues][:percentage]).round()
+      monthly_expenses_sum[:total] = ((mortgage_principal[:total] + mortgage_interest[:total] + property_tax[:total] + home_insurance[:total] + @pmi_insurance[:total])) rescue 0.0
 
-      return monthly_breakdown
+      mortgage_principal[:percentage] = ((mortgage_principal[:monthly]*100 / monthly_expenses_sum[:monthly])).round(2) rescue 0.0
+
+      mortgage_interest[:percentage] =  ((mortgage_interest[:monthly]*100 / monthly_expenses_sum[:monthly])).round(2) rescue 0.0
+
+      property_tax[:percentage] = ((property_tax[:monthly]*100 / monthly_expenses_sum[:monthly])).round(2) rescue 0.0
+
+      home_insurance[:percentage] = ((home_insurance[:monthly]*100 / monthly_expenses_sum[:monthly])).round(2) rescue 0.0
+
+      @pmi_insurance[:percentage] =  ((@pmi_insurance[:monthly]*100 / monthly_expenses_sum[:monthly])).round(2) rescue 0.0
+
+      hoa_dues[:percentage] =  ((hoa_dues[:monthly]*100 / monthly_expenses_sum[:monthly])).round(2) rescue 0.0
+
+      monthly_expenses_sum[:percentage] = (mortgage_principal[:percentage] + mortgage_interest[:percentage] + property_tax[:percentage] + home_insurance[:percentage] + @pmi_insurance[:percentage] + hoa_dues[:percentage]).round() rescue 100
+
+      return { mortgage_principal: mortgage_principal,mortgage_interest: mortgage_interest,home_insurance: home_insurance,pmi_insurance: @pmi_insurance,hoa_dues: hoa_dues,monthly_expenses_sum: monthly_expenses_sum, property_tax: property_tax}
     end
 
     def calculate_pmi_term(home_price, number_of_payments, down_payment)
